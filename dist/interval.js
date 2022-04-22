0304e173de05e6f6bf3d83d4687d2566db4f2441
Dmitry Vibe <riim@yandex.ru>
2022-04-22 15:32:01 +0300

    v1.1.0

--- dist/interval.js
+++ dist/interval.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.interval30d = exports.interval15d = exports.interval2w = exports.interval10d = exports.interval1w = exports.interval5d = exports.interval4d = exports.interval3d = exports.interval2d = exports.interval1d = exports.interval12h = exports.interval10h = exports.interval6h = exports.interval5h = exports.interval4h = exports.interval3h = exports.interval2h = exports.interval1h = exports.interval30m = exports.interval25m = exports.interval20m = exports.interval15m = exports.interval10m = exports.interval5m = exports.interval4m = exports.interval3m = exports.interval2m = exports.interval1m = exports.interval30s = exports.interval25s = exports.interval20s = exports.interval15s = exports.interval10s = exports.interval5s = exports.interval4s = exports.interval3s = exports.interval2s = exports.interval1s = exports.interval = void 0;
+exports.interval = void 0;
 const UNITS = [
     's',
     'second',
@@ -19,7 +19,7 @@ const UNITS = [
     'weaks'
 ];
 const reValue = new RegExp(`^(\\d+)(${UNITS.join('|')})$`);
-function interval(value, unit) {
+exports.interval = function interval(value, unit) {
     if (typeof value == 'string') {
         if (!reValue.test(value)) {
             throw SyntaxError('Invalid value format');
@@ -55,43 +55,47 @@ function interval(value, unit) {
         }
     }
     return value;
-}
-exports.interval = interval;
-exports.interval1s = interval(1, 's');
-exports.interval2s = interval(2, 's');
-exports.interval3s = interval(3, 's');
-exports.interval4s = interval(4, 's');
-exports.interval5s = interval(5, 's');
-exports.interval10s = interval(10, 's');
-exports.interval15s = interval(15, 's');
-exports.interval20s = interval(20, 's');
-exports.interval25s = interval(25, 's');
-exports.interval30s = interval(30, 's');
-exports.interval1m = interval(1, 'm');
-exports.interval2m = interval(2, 'm');
-exports.interval3m = interval(3, 'm');
-exports.interval4m = interval(4, 'm');
-exports.interval5m = interval(5, 'm');
-exports.interval10m = interval(10, 'm');
-exports.interval15m = interval(15, 'm');
-exports.interval20m = interval(20, 'm');
-exports.interval25m = interval(25, 'm');
-exports.interval30m = interval(30, 'm');
-exports.interval1h = interval(1, 'h');
-exports.interval2h = interval(2, 'h');
-exports.interval3h = interval(3, 'h');
-exports.interval4h = interval(4, 'h');
-exports.interval5h = interval(5, 'h');
-exports.interval6h = interval(6, 'h');
-exports.interval10h = interval(10, 'h');
-exports.interval12h = interval(12, 'h');
-exports.interval1d = interval(1, 'd');
-exports.interval2d = interval(2, 'd');
-exports.interval3d = interval(3, 'd');
-exports.interval4d = interval(4, 'd');
-exports.interval5d = interval(5, 'd');
-exports.interval1w = interval(1, 'w');
-exports.interval10d = interval(10, 'd');
-exports.interval2w = interval(2, 'w');
-exports.interval15d = interval(15, 'd');
-exports.interval30d = interval(30, 'd');
+};
+exports.default = exports.interval;
+exports.interval.s1 = (0, exports.interval)(1, 's');
+exports.interval.s2 = (0, exports.interval)(2, 's');
+exports.interval.s3 = (0, exports.interval)(3, 's');
+exports.interval.s4 = (0, exports.interval)(4, 's');
+exports.interval.s5 = (0, exports.interval)(5, 's');
+exports.interval.s10 = (0, exports.interval)(10, 's');
+exports.interval.s15 = (0, exports.interval)(15, 's');
+exports.interval.s20 = (0, exports.interval)(20, 's');
+exports.interval.s25 = (0, exports.interval)(25, 's');
+exports.interval.s30 = (0, exports.interval)(30, 's');
+exports.interval.m1 = (0, exports.interval)(1, 'm');
+exports.interval.m2 = (0, exports.interval)(2, 'm');
+exports.interval.m3 = (0, exports.interval)(3, 'm');
+exports.interval.m4 = (0, exports.interval)(4, 'm');
+exports.interval.m5 = (0, exports.interval)(5, 'm');
+exports.interval.m10 = (0, exports.interval)(10, 'm');
+exports.interval.m15 = (0, exports.interval)(15, 'm');
+exports.interval.m20 = (0, exports.interval)(20, 'm');
+exports.interval.m25 = (0, exports.interval)(25, 'm');
+exports.interval.m30 = (0, exports.interval)(30, 'm');
+exports.interval.h1 = (0, exports.interval)(1, 'h');
+exports.interval.h2 = (0, exports.interval)(2, 'h');
+exports.interval.h3 = (0, exports.interval)(3, 'h');
+exports.interval.h4 = (0, exports.interval)(4, 'h');
+exports.interval.h5 = (0, exports.interval)(5, 'h');
+exports.interval.h6 = (0, exports.interval)(6, 'h');
+exports.interval.h10 = (0, exports.interval)(10, 'h');
+exports.interval.h12 = (0, exports.interval)(12, 'h');
+exports.interval.h16 = (0, exports.interval)(16, 'h');
+exports.interval.h20 = (0, exports.interval)(20, 'h');
+exports.interval.d1 = (0, exports.interval)(1, 'd');
+exports.interval.d2 = (0, exports.interval)(2, 'd');
+exports.interval.d3 = (0, exports.interval)(3, 'd');
+exports.interval.d4 = (0, exports.interval)(4, 'd');
+exports.interval.d5 = (0, exports.interval)(5, 'd');
+exports.interval.w1 = (0, exports.interval)(1, 'w');
+exports.interval.d10 = (0, exports.interval)(10, 'd');
+exports.interval.w2 = (0, exports.interval)(2, 'w');
+exports.interval.d15 = (0, exports.interval)(15, 'd');
+exports.interval.d20 = (0, exports.interval)(20, 'd');
+exports.interval.d25 = (0, exports.interval)(25, 'd');
+exports.interval.d30 = (0, exports.interval)(30, 'd');
--- src/interval.ts
+++ src/interval.ts
@@ -20,9 +20,55 @@ export type TUnit = typeof UNITS[number];
 
 const reValue = new RegExp(`^(\\d+)(${UNITS.join('|')})$`);
 
-export function interval(value: number, unit: TUnit): number;
-export function interval(value: `${number}${TUnit}`): number;
-export function interval(value: number | string, unit?: TUnit) {
+interface IInterval {
+	(value: number, unit: TUnit): number;
+	(value: `${number}${TUnit}`): number;
+
+	s1: number;
+	s2: number;
+	s3: number;
+	s4: number;
+	s5: number;
+	s10: number;
+	s15: number;
+	s20: number;
+	s25: number;
+	s30: number;
+	m1: number;
+	m2: number;
+	m3: number;
+	m4: number;
+	m5: number;
+	m10: number;
+	m15: number;
+	m20: number;
+	m25: number;
+	m30: number;
+	h1: number;
+	h2: number;
+	h3: number;
+	h4: number;
+	h5: number;
+	h6: number;
+	h10: number;
+	h12: number;
+	h16: number;
+	h20: number;
+	d1: number;
+	d2: number;
+	d3: number;
+	d4: number;
+	d5: number;
+	w1: number;
+	d10: number;
+	w2: number;
+	d15: number;
+	d20: number;
+	d25: number;
+	d30: number;
+}
+
+export const interval: IInterval = function interval(value: number | string, unit?: TUnit) {
 	if (typeof value == 'string') {
 		if (!reValue.test(value)) {
 			throw SyntaxError('Invalid value format');
@@ -61,43 +107,49 @@ export function interval(value: number | string, unit?: TUnit) {
 	}
 
 	return value;
-}
+} as any;
 
-export const interval1s = interval(1, 's');
-export const interval2s = interval(2, 's');
-export const interval3s = interval(3, 's');
-export const interval4s = interval(4, 's');
-export const interval5s = interval(5, 's');
-export const interval10s = interval(10, 's');
-export const interval15s = interval(15, 's');
-export const interval20s = interval(20, 's');
-export const interval25s = interval(25, 's');
-export const interval30s = interval(30, 's');
-export const interval1m = interval(1, 'm');
-export const interval2m = interval(2, 'm');
-export const interval3m = interval(3, 'm');
-export const interval4m = interval(4, 'm');
-export const interval5m = interval(5, 'm');
-export const interval10m = interval(10, 'm');
-export const interval15m = interval(15, 'm');
-export const interval20m = interval(20, 'm');
-export const interval25m = interval(25, 'm');
-export const interval30m = interval(30, 'm');
-export const interval1h = interval(1, 'h');
-export const interval2h = interval(2, 'h');
-export const interval3h = interval(3, 'h');
-export const interval4h = interval(4, 'h');
-export const interval5h = interval(5, 'h');
-export const interval6h = interval(6, 'h');
-export const interval10h = interval(10, 'h');
-export const interval12h = interval(12, 'h');
-export const interval1d = interval(1, 'd');
-export const interval2d = interval(2, 'd');
-export const interval3d = interval(3, 'd');
-export const interval4d = interval(4, 'd');
-export const interval5d = interval(5, 'd');
-export const interval1w = interval(1, 'w');
-export const interval10d = interval(10, 'd');
-export const interval2w = interval(2, 'w');
-export const interval15d = interval(15, 'd');
-export const interval30d = interval(30, 'd');
+export default interval;
+
+interval.s1 = interval(1, 's');
+interval.s2 = interval(2, 's');
+interval.s3 = interval(3, 's');
+interval.s4 = interval(4, 's');
+interval.s5 = interval(5, 's');
+interval.s10 = interval(10, 's');
+interval.s15 = interval(15, 's');
+interval.s20 = interval(20, 's');
+interval.s25 = interval(25, 's');
+interval.s30 = interval(30, 's');
+interval.m1 = interval(1, 'm');
+interval.m2 = interval(2, 'm');
+interval.m3 = interval(3, 'm');
+interval.m4 = interval(4, 'm');
+interval.m5 = interval(5, 'm');
+interval.m10 = interval(10, 'm');
+interval.m15 = interval(15, 'm');
+interval.m20 = interval(20, 'm');
+interval.m25 = interval(25, 'm');
+interval.m30 = interval(30, 'm');
+interval.h1 = interval(1, 'h');
+interval.h2 = interval(2, 'h');
+interval.h3 = interval(3, 'h');
+interval.h4 = interval(4, 'h');
+interval.h5 = interval(5, 'h');
+interval.h6 = interval(6, 'h');
+interval.h10 = interval(10, 'h');
+interval.h12 = interval(12, 'h');
+interval.h16 = interval(16, 'h');
+interval.h20 = interval(20, 'h');
+interval.d1 = interval(1, 'd');
+interval.d2 = interval(2, 'd');
+interval.d3 = interval(3, 'd');
+interval.d4 = interval(4, 'd');
+interval.d5 = interval(5, 'd');
+interval.w1 = interval(1, 'w');
+interval.d10 = interval(10, 'd');
+interval.w2 = interval(2, 'w');
+interval.d15 = interval(15, 'd');
+interval.d20 = interval(20, 'd');
+interval.d25 = interval(25, 'd');
+interval.d30 = interval(30, 'd');
